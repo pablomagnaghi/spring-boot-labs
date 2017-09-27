43dd25403ee244029f30f7d1b305737cea3b3ded
pablomagnaghi <pablomagnaghi@gmail.com>
2017-09-26 22:15:35 -0300

    02-externalized-configuration

---
+++ 02-externalized-configuration/property-file/src/main/java/io/pivotal/workshop/GreetingController.java
@@ -0,0 +1,15 @@
+package io.pivotal.workshop;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @Value("${custom.message}")
+    private String message;
+    @GetMapping("/")
+    public String greeting() {
+        return message;
+    }
+}
