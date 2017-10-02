124562ce19746016cb20e52e5ef0eb8729522844
Pablo Magnaghi <pmagnaghi@solstice.com>
2017-10-02 01:35:37 -0300

    04-data

---
+++ 04-data/code-snippet-manager-jpa/src/main/java/io/pivotal/workshop/CodeSnippetManagerJpaApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSnippetManagerJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSnippetManagerJpaApplication.class, args);
+	}
+}
---
+++ 04-data/code-snippet-manager-jpa/src/main/java/io/pivotal/workshop/SnippetPresenter.java
@@ -0,0 +1,16 @@
+package io.pivotal.workshop;
+import org.springframework.stereotype.Component;
+import java.time.format.DateTimeFormatter;
+@Component
+public class SnippetPresenter {
+    DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
+    public SnippetInfo present(SnippetRecord record) {
+        return new SnippetInfo(
+                record.id,
+                record.title,
+                record.code,
+                record.created.format(formatter),
+                record.modified.format(formatter)
+        );
+    }
+}
